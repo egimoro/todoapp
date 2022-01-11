9ac1c96dd1665ac35a123750c60701dcfe86a175
egimoro <gimoremma@tutanota.com>
2022-01-11 22:58:24 +0100

    Completed Todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos.vue';
+import AddTodo from './components/AddTodo.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  name: 'App',
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "todo 2",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "todo 3",
+          completed: false
+        },
+        {
+          id: 4,
+          title: "todo 4",
+          completed: true
+
+        }
+      ]
+    }
+  },
+  components: {
+    Todos,
+    AddTodo
+  },
+  methods: {
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo]
+    },
+    deleteTodo(id){
+      this.todos = this.todos.filter((todo)=>todo.id !== id)
+    }
+  }
+  
+};
+</script>
